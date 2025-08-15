f341af345d91989311877ac4cb2079ac664a172a
Tomasz Okoń <tomekokon75@gmail.com>
2025-08-15 17:56:15 +0200

    Lexer's peek advance and is at the end methods

--- src/Lexer.cpp
+++ src/Lexer.cpp
@@ -1,12 +1,15 @@
 #include "miniC/Lexer.hpp"
 
-
 namespace minic
 {
 
 Lexer::Lexer(const std::string& source)
+    : source_(source)
+    , pos_(0)
+    , line_(1)
+    , column_(1)
 {
-    // TODO: Implement constructor
+    indent_levels_.push(0); // Start with an initial indentation level of 0
 }
 
 std::vector<minic::Token> Lexer::Lex()
@@ -17,26 +20,42 @@ std::vector<minic::Token> Lexer::Lex()
 
 char Lexer::peek() const
 {
-    // TODO: Implement peek
+    if (pos_ < source_.size())
+    {
+        return source_[pos_];
+    }
+    // Return null character if at end of input
     return '\0';
 }
 
 char Lexer::advance()
 {
-    // TODO: Implement advance
+    if (pos_ < source_.size())
+    {
+        char current = source_[pos_++];
+        if (current == '\n')
+        {
+            line_++;
+            column_ = 1;
+        }
+        else
+        {
+            column_++;
+        }
+        return current;
+    }
     return '\0';
 }
 
 bool Lexer::is_at_end() const
 {
-    // TODO: Implement is_at_end
-    return true;
+    return pos_ >= source_.size();
 }
 
 Token Lexer::next_token()
 {
     // TODO: Implement next_token
-    return Token{};
+    return Token {};
 }
 
 void Lexer::skip_whitespace()
@@ -52,31 +71,31 @@ void Lexer::skip_comment()
 Token Lexer::scan_identifier()
 {
     // TODO: Implement scan_identifier
-    return Token{};
+    return Token {};
 }
 
 Token Lexer::scan_number()
 {
     // TODO: Implement scan_number
-    return Token{};
+    return Token {};
 }
 
 Token Lexer::scan_string()
 {
     // TODO: Implement scan_string
-    return Token{};
+    return Token {};
 }
 
 Token Lexer::handle_indentation()
 {
     // TODO: Implement handle_indentation
-    return Token{};
+    return Token {};
 }
 
 Token Lexer::make_token(TokenType type, const std::variant<int, std::string>& value) const
 {
     // TODO: Implement make_token
-    return Token{};
+    return Token {};
 }
 
 void Lexer::check_indent_consistency(char c)
